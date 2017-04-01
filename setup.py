0410c05c432e944c78a5c416d098638380dd1c5e
Geoff Ford <gford1000@me.com>
2017-04-02 09:29:23 +1000

    PiPY setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'awssl',
+  packages = ['awssl'], 
+  version = '0.1',
+  description = 'Classes to generate ASL for AWS Step Functions',
+  author = 'Geoff Ford',
+  author_email = 'gford1000@hotmail.com',
+  url = 'https://github.com/gford1000/awssl', 
+  download_url = 'https://github.com/gford1000/awssl/archive/0.1.tar.gz', 
+  keywords = ['ASL', 'AWS', 'Step Functions'], 
+  classifiers = [],
+)
