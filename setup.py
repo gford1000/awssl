3c62ed03e64c032e75a18dd484f3bc2cc1a76535
Geoff Ford <gford1000@me.com>
2017-04-13 18:24:27 +1000

    Incremented the awssl package to 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'awssl',
   packages = ['awssl'], 
-  version = '0.1',
+  version = '0.2',
   description = 'Classes to generate ASL for AWS Step Functions',
   author = 'Geoff Ford',
   author_email = 'gford1000@hotmail.com',
   url = 'https://github.com/gford1000/awssl', 
-  download_url = 'https://github.com/gford1000/awssl/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/gford1000/awssl/archive/0.2.tar.gz', 
   keywords = ['ASL', 'AWS', 'Step Functions'], 
   classifiers = [],
 )
